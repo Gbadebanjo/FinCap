aeb573a39685e5b74359c984f1c878f6d5a7682b
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2024-04-26 02:40:32 +0100

    Button and prettier added

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  printWidth: 80,
+};
--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StyledButton from './app/components/StyledButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <StyledButton title="Click Me" />
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#',
+    backgroundColor: 'tomato',
     color: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
