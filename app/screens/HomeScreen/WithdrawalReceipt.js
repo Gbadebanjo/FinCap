242978afd246554f7735f957960553c2cd7517f9
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2024-07-20 17:12:08 +0100

    Receipt UI Added

--- app/screens/HomeScreen/WithdrawalReceipt.js
+++ app/screens/HomeScreen/WithdrawalReceipt.js
@@ -4,65 +4,65 @@ import { AntDesign } from '@expo/vector-icons';
 import React from 'react'
 import StyledButton from '../../components/StyledButton';
 
-const WithdrawalReceipt = () => {
-  return (
-    <SafeAreaView>
-        <TouchableOpacity
+const WithdrawalReceipt = ({ navigation }) => {
+    return (
+        <SafeAreaView>
+            <TouchableOpacity
                 style={styles.icon}
-                onPress={() => navigation.navigate('WithdrawalInput')}>
+                onPress={() => navigation.navigate('Home')}>
                 <AntDesign name="left" size={16} color="black" />
             </TouchableOpacity>
-      <Text style={styles.heading}>Withdrawal</Text>
-      <Text style={styles.headingAmount}>$490.00</Text>
-        <Text style={styles.headingMessage}>Transaction successful</Text>
-        <View style={styles.detailsContainer}>
-            <View style={styles.details}>
-                <Text style={styles.detailsText}>Transaction Amount</Text>
-                <Text style={styles.detailsValue}>$490.00</Text>
+            <Text style={styles.heading}>Withdrawal</Text>
+            <Text style={styles.headingAmount}>$490.00</Text>
+            <Text style={styles.headingMessage}>Transaction successful</Text>
+            <View style={styles.detailsContainer}>
+                <View style={styles.details}>
+                    <Text style={styles.detailsText}>Transaction Amount</Text>
+                    <Text style={styles.detailsValue}>$490.00</Text>
+                </View>
+                <View style={styles.details}>
+                    <Text style={styles.detailsText}>Transaction Date</Text>
+                    <Text style={styles.detailsValue}>2024-07-02 12:15:14</Text>
+                </View>
+                <View style={styles.details}>
+                    <Text style={styles.detailsText}>Sender</Text>
+                    <Text style={styles.detailsValue}>Timilehin George</Text>
+                </View>
+                <View style={styles.details}>
+                    <Text style={styles.detailsText}>Recipient</Text>
+                    <Text style={styles.detailsValue}>Timilehin George</Text>
+                </View>
+                <View style={styles.details}>
+                    <Text style={styles.detailsText}>Fee</Text>
+                    <Text style={styles.detailsValue}>$10</Text>
+                </View>
+                <View style={styles.details}>
+                    <Text style={styles.detailsText}>Narration</Text>
+                    <Text style={styles.detailsValue}>Nil</Text>
+                </View>
+                <View style={styles.details}>
+                    <Text style={styles.detailsText}>Reference Code</Text>
+                    <Text style={styles.detailsValue}>ygfhokjjhvghok;kljhjh</Text>
+                </View>
+                <View style={styles.details}>
+                    <Text style={styles.detailsText}>Transaction ID</Text>
+                    <Text style={styles.detailsValue}>000000HGV89JHJ</Text>
+                </View>
+                <View style={styles.details}>
+                    <Text style={styles.detailsText}>Status</Text>
+                    <Text style={styles.detailsValue}>Successful</Text>
+                </View>
             </View>
-            <View style={styles.details}>
-                <Text style={styles.detailsText}>Transaction Date</Text>
-                <Text style={styles.detailsValue}>2024-07-02 12:15:14</Text>
-            </View>
-            <View style={styles.details}>
-                <Text style={styles.detailsText}>Sender</Text>
-                <Text style={styles.detailsValue}>Timilehin George</Text>
-            </View>
-            <View style={styles.details}>
-                <Text style={styles.detailsText}>Recipient</Text>
-                <Text style={styles.detailsValue}>Timilehin George</Text>
-            </View>
-            <View style={styles.details}>
-                <Text style={styles.detailsText}>Fee</Text>
-                <Text style={styles.detailsValue}>$10</Text>
-            </View>
-            <View style={styles.details}>
-                <Text style={styles.detailsText}>Narration</Text>
-                <Text style={styles.detailsValue}>Nil</Text>
-            </View>
-            <View style={styles.details}>
-                <Text style={styles.detailsText}>Reference Code</Text>
-                <Text style={styles.detailsValue}>ygfhokjjhvghok;kljhjh</Text>
-            </View>
-            <View style={styles.details}>
-                <Text style={styles.detailsText}>Transaction ID</Text>
-                <Text style={styles.detailsValue}>000000HGV89JHJ</Text>
-            </View>
-            <View style={styles.details}>
-                <Text style={styles.detailsText}>Status</Text>
-                <Text style={styles.detailsValue}>Successful</Text>
-            </View>
-        </View>
-        <View style={styles.buttons}>
+            <View style={styles.buttons}>
                 <StyledButton
-                    title='Submit'
+                    title='Share Receipt'
                     // onPress={handleSubmit}
                     width="90%"
                     marginLeft="5%"
                     marginTop={20}
                 />
                 <StyledButton
-                    title="Re-upload"
+                    title="Done"
                     width="90%"
                     marginLeft="5%"
                     marginTop={20}
@@ -72,8 +72,8 @@ const WithdrawalReceipt = () => {
                     TextColor="#7538EC"
                 />
             </View>
-    </SafeAreaView>
-  )
+        </SafeAreaView>
+    )
 }
 
 export default WithdrawalReceipt
@@ -82,36 +82,55 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingHorizontal: 20,
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
     },
     icon: {
-        paddingTop: 30,
+        // paddingTop: 20,
+        margin: 20,
         width: 30,
-        marginBottom: 20
     },
     heading: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 5
+        fontSize: 16,
+        fontWeight: '500',
+        textAlign: 'center',
+        marginTop: 10
     },
     headingAmount: {
-        fontSize: 14,
-        marginBottom: 30
+        fontSize: 40,
+        marginBottom: 10,
+        fontWeight: '700',
+        textAlign: 'center'
     },
     headingMessage: {
-        fontSize: 14,
+        fontSize: 16,
         marginBottom: 30,
-        color: 'green'
+        backgroundColor: '#27AE60',
+        color: 'white',
+        padding: 20,
+        textAlign: 'center',
+        width: 250,
+        alignSelf: 'center',
+        borderRadius: 30,
+        overflow: 'hidden',
     },
     detailsContainer: {
-        marginVertical: 20,
-        paddingBottom: 20,
-        borderBottomColor: '#BBBBBD',
-        borderBottomWidth: 1,
+        marginHorizontal: 20,
+    },
+    details: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginVertical: 5,
+    },
+    detailsText: {
+        fontSize: 16,
+        color: '#A5A5A5',
+    },
+    detailsValue: {
+        fontSize: 14,
+        color: '#000',
+        fontWeight: '500',
     },
     buttons: {
-        flex: 1,
         justifyContent: 'flex-end',
-        marginBottom: 20,
     },
 })
